13700b159eba62b35a2b08c6dfaf42406e87b0aa
edwjlr <edwjlr@gmail.com>
2025-01-13 09:01:27 -0800

    delete files

--- Projects/rec_face/dlib_install/upload_to_gcloud.py
+++
@@ -1,9 +0,0 @@
-import pickle
-
-# Load face encodings
-with open('face_encodings.pkl', 'rb') as f:
-    known_face_encodings = pickle.load(f)
-
-# Assuming `known_face_encodings` is a dict with person names as keys and their encodings as values
-names = list(known_face_encodings.keys())
-encodings = list(known_face_encodings.values())
